c0e6bf1d576cfb84d8cf6dd336a36731c10ad554
Kennedy Kong <kennedy.kong@gmail.com>
2017-03-08 21:06:16 -0500

    trying out unittest

--- 1.py
+++ 1.py
@@ -7,6 +7,6 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 def main():
     three = set([i for i in range(3, 1000, 3)] + [i for i in range(5, 1000, 5)])
-    return sum(three)
+    return int(sum(three))
 
 if __name__ == "__main__": main()
--- test_euler.py
+++ test_euler.py
@@ -4,3 +4,15 @@ import unittest
 class KenTest(unittest.TestCase):
     def test_something(self):
         self.assertIsInstance(1, int)
+
+    def test_1(self):
+        euler = __import__('1')
+        self.assertIsInstance(euler.main(), int)
+
+    def test_2(self):
+        euler = __import__('2')
+        self.assertIsInstance(euler.main(), int)
+
+
+if __name__ == '__main__':
+    unittest.main()
